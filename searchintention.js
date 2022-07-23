1786070db3bdb0ebb9038945b68457d66eead7d8
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2022-07-23 18:33:05 -0300

    add event parameter

--- searchintention.js
+++ searchintention.js
@@ -1,7 +1,7 @@
 const telegram = require('./telegram')
 const repository = require('./accorrepository');
 
-exports.search = async () => {
+exports.search = async (event) => {
     let tableData = await repository.searchAllDataExperiences();
     for (let item of tableData) {
         let text = "<b>Resultado da Busca</b> \u{1F50D} \u{1F50D} \u{1F50D} \n\n<b>Nome do Evento:</b> " + item.name + "\n\n<b>Pontos Necessários:</b> \u{1F4B0} " + item.price + "\n\n<b>Link:</b> \u{1F310} " + item.link + "\n";
