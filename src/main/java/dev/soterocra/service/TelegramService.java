42be47fdb768382796ed1d8cc6a0fd9933efaa47
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2022-12-04 22:51:34 -0300

    improve messages of add and remove

--- src/main/java/dev/soterocra/service/TelegramService.java
+++ src/main/java/dev/soterocra/service/TelegramService.java
@@ -39,14 +39,23 @@ public class TelegramService implements ItemObserver {
 
         userService.findAll().parallelStream()
                 .forEach(user -> {
-                    Log.info("Enviando mensagem para usuário: " + user);
-                    var text = "<b>Resultado da Busca</b> \\u{1F50D} \\u{1F50D} \\u{1F50D} \\n\\n<b>Nome do Evento:</b> " + item.getName() + "\\n\\n<b>Pontos Necessários:</b> \\u{1F4B0} " + item.getPrice() + "\\n\\n<b>Link:</b> \\u{1F310} " + item.getLink() + "\n";
+                    Log.info("Enviando mensagem de adição para usuário: " + user);
+//                    var text = "<b>Resultado da Busca</b> \uD83D\uDD0D \uD83D\uDD0D \uD83D\uDD0D \n\n<b>Nome do Evento:</b> " + item.getName() + "\n\n<b>Pontos Necessários:</b> \uD83D\uDCB0 " + item.getPrice() + "\n\n<b>Link:</b> \uD83C\uDF10 " + item.getLink() + "\n";
+                    var text = "<b>Adição</b> ✅ ✅ ✅ \n\n<b>Nome do Evento:</b> " + item.getName() + "\n\n<b>Pontos Necessários:</b> \uD83D\uDCB0 " + item.getPrice() + "\n\n<b>Link:</b> \uD83C\uDF10 " + item.getLink() + "\n";
                     client.sendMessage(botToken, new SendMessageObject(user.getChatId(), text));
                 });
     }
 
     @Override
     public void removedItem(Item item) {
+        Log.info("Item para remoção recebido no observer do telegram. " + item);
 
+        userService.findAll().parallelStream()
+                .forEach(user -> {
+                    Log.info("Enviando mensagem de remoção para usuário: " + user);
+//                    var text = "<b>Resultado da Busca</b> \uD83D\uDD0D \uD83D\uDD0D \uD83D\uDD0D \n\n<b>Nome do Evento:</b> " + item.getName() + "\n\n<b>Pontos Necessários:</b> \uD83D\uDCB0 " + item.getPrice() + "\n\n<b>Link:</b> \uD83C\uDF10 " + item.getLink() + "\n";
+                    var text = "<b>Adição</b> ❌ ❌ ❌ \n\n<b>Nome do Evento:</b> " + item.getName() + "\n\n<b>Pontos Necessários:</b> \uD83D\uDCB0 " + item.getPrice() + "\n\n<b>Link:</b> \uD83C\uDF10 " + item.getLink() + "\n";
+                    client.sendMessage(botToken, new SendMessageObject(user.getChatId(), text));
+                });
     }
 }
